977b6132d826d86ac127d11b59b442ce97096e47
VajskiDs <87033268+L10N37@users.noreply.github.com>
2022-11-26 17:36:37 +1100

    Update prototype.ino

--- Experimental_single-wire_SUB.Q/prototype.ino
+++ Experimental_single-wire_SUB.Q/prototype.ino
@@ -1,17 +1,49 @@
-#define subqislow (PIND == 0B000000000)
-int bitdelay(3970);
+// experimental
+// stupid MCU's have a lot of weird quirks
+// seems to detect TOC area reliably, injections happen (as it's monitored) but no black screen.
+// 
+#define subqislow (PIND == 0B00000000)
 #define wait delayMicroseconds(bitdelay);
+#define LowBit bitWrite(DDRD, 4, 1), delayMicroseconds(bitdelay)
+#define HighBit bitClear(DDRD, 4), delayMicroseconds(bitdelay)
+#define EndOfMagicKey injectcounter++, delay(stringdelay)
+int injectcounter = 0x00;
+int bitdelay(3970);
+int stringdelay(160);  //delay between string injections
 int capturetime = 0;
 int fallingedges = 0;
 int risingedges = 0;
 int timer = 0;
 
 
+char ALLREGIONS[] = "10011010100100111101001010111010010101110100S10011010100100111101001010111010010111110100S10011010100100111101001010111010010110110100";
+char SCEE[] = "10011010100100111101001010111010010101110100";
+char SCEA[] = "10011010100100111101001010111010010111110100";
+char SCEI[] = "10011010100100111101001010111010010110110100";
+
+
+
+//***************** MAGIC KEYS *****************************
+// EUROPE / AUSTRALIA / NEWZEALAND = SCEE
+// USA = SCEA
+// JP = SCEI
+// ALL= ALLREGIONS
+//**********************************************************
+
+
+#define SELECT_MAGICKEY SCEE  //<---------------------------------------------REGION SELECT!! ENTER CONSOLE REGION
+
+
+
+
 void setup() {
-  DDRD = 0B00000000;
-  PORTD = 0B00000000;
-  bitClear(DDRD, 2);
-  Serial.begin(19200);  // enable serial monitor as a type of debug console
+
+  Serial.begin(115200);  // enable serial monitor as a type of debug console
+  monitorpinstates();
+}
+
+void monitorpinstates() {
+  bitClear(DDRD, 4);      //  high-z data
 }
 
 
@@ -26,15 +58,16 @@ void resetvariables() {
   capturetime = 0;
   fallingedges = 0;
   risingedges = 0;
+  injectcounter = 0;
 }
 
 void subqrising() {
 
   capturetime++;
   risingedges++;
-  
 
-    switch (risingedges) {
+
+  switch (risingedges) {
     case 1:
       wait break;
     case 2:
@@ -58,7 +91,7 @@ void subqfalling() {
     case 1:
       wait wait wait wait wait break;
     case 2:
-      wait wait wait wait wait wait wait wait wait break;
+     wait wait wait wait wait wait wait wait wait break;
     case 3:
       wait break;
     case 4:
@@ -71,25 +104,57 @@ void print() {
 }
 
 
+void inject() {
+
+
+  bitWrite(DDRD, 4, 1);  //  set data as output for injections
+  bitClear(PORTD, 4);     // set data low
+
+  do {
+
+    for (byte i = 0; i < sizeof(SELECT_MAGICKEY); i++)
+
+      switch (SELECT_MAGICKEY[i]) {
+        case '1':
+          HighBit;   Serial.print(injectcounter);
+          break;
+        case '0':
+          LowBit;   Serial.print(injectcounter);
+          break;
+      }
+
+    EndOfMagicKey;
+
+  } while (injectcounter <= 3);
+}
+
 void loop() {
 
-  while (subqislow) { ; }  //wait for activity on Sub.Q
-
- if (capturetime == 0) attachinterrupts();  //attach interrupts to D2 and enable interrupts
-
+  
+  if (capturetime >=10 ) {capturetime = 0; noInterrupts(); wait}
+  attachinterrupts();  //attach interrupts to D2 and enable interrupts
 
 
 
   // 0xA0
   if (capturetime == 8 && risingedges == 4 && fallingedges == 4) {
+    noInterrupts();
     print();
+    inject();
+    monitorpinstates();
     resetvariables();
+    
+
   }
 
   // 0xA1 and 0xA2
   else if (capturetime == 10 && risingedges == 5 && fallingedges == 5) {
+    noInterrupts();
     print();
+    inject();
+    monitorpinstates();
     resetvariables();
+    
   }
 
 
